3f1207c72a9cba931d2f7cb664c8da3c47495a31
Asim1O1 <146157042+Asim1O1@users.noreply.github.com>
2024-09-06 10:22:56 +0545

    api thunk for register

--- frontend/src/redux/slices/user/authSlice.jsx
+++ frontend/src/redux/slices/user/authSlice.jsx
@@ -13,7 +13,45 @@ export const registerUser = createAsyncThunk(
   }
 );
 
-export const loginUser = createAsyncThunk("LOGIN_USER", async () => {
-  try {
-  } catch (error) {}
+// export const loginUser = createAsyncThunk("LOGIN_USER", async () => {
+//   try {
+//   } catch (error) {}
+// });
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState: {
+    user: null,
+    loading: false,
+    error: null,
+    success: false,
+  },
+  reducers: {
+    resetAuthState: (state) => {
+      state.loading = false;
+      state.error = null;
+      state.success = false;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(registerUser.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+        state.success = false;
+      })
+      .addCase(registerUser.fulfilled, (state, action) => {
+        state.loading = false;
+        state.success = true;
+        state.user = action?.payload;
+      })
+
+      .addCase(registerUser.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action?.payload;
+      });
+  },
 });
+
+export const { resetAuthState } = authSlice.actions;
+export default authSlice.reducer;
