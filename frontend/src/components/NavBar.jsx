ea3d39c2307839210d3356b0e82704d8368260e9
Asim1O1 <146157042+Asim1O1@users.noreply.github.com>
2024-09-14 15:26:45 +0545

    navbar design

---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+function NavBar() {
+  return (
+    <>
+      <nav className="  w-full max-w-3xl bg-gray-500 h-16 mx-auto rounded-3xl  place-content-center sticky z-50 top-0">
+        <ul className="flex justify-around text-white cursor-pointer font-semibold font-mono">
+          <li>
+            <a href="/" className="hover:text-blue-500">
+              Home
+            </a>
+          </li>
+
+          <li>
+            {" "}
+            <a href="/register" className="hover:text-blue-500">
+              Register
+            </a>
+          </li>
+          <li>
+            {" "}
+            <a href="/login" className="hover:text-blue-500">
+              Login
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+}
+
+export default NavBar;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
+import NavBar from "../components/NavBar";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <>
+      <div>Home</div>
+      <NavBar />
+    </>
+  );
+};
 
-export default Home
+export default Home;
